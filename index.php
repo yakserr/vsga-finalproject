4c27abe8c82f36e1e988516a18c0bdaab8a41d17
Yassir Ah <yassirnugroho48@gmail.com>
2022-08-13 21:48:43 +0700

    welcome page

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
+session_start();
+
+// include config.php
 include_once 'config.php';
+
 ?>
 
 <!doctype html>
@@ -9,7 +13,7 @@ include_once 'config.php';
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>VSGA Final Project!</title>
+    <title><?= APP_NAME ?></title>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
@@ -18,16 +22,20 @@ include_once 'config.php';
 </head>
 
 <body>
+    <!-- Hero bar -->
     <div class="hero">
+        <!-- Header  -->
         <nav class="navbar navbar-expand-lg navbar-light shadow-lg justify-content-between">
-            <a class="navbar-brand text-light" href="#">Perpus</a>
+            <a class="navbar-brand text-light" href="index.php">Perpus</a>
 
             <div class="navbar-nav">
                 <a class="nav-link text-light" href="src/auth/login.php">Login</a>
             </div>
         </nav>
+        <!-- End Header  -->
 
-        <div class="introduction ml-4 p-1 text-left text-white rounded-lg">
+        <!-- Section introduction and search bar  -->
+        <div class="introduction ml-5 p-1 text-left text-white rounded-lg">
             <h1>Welcome to Perpus!</h1>
             <p>a website that provides borrowing, booking and reading books online</p>
             <div class="search-box">
@@ -37,11 +45,14 @@ include_once 'config.php';
                 </form>
             </div>
         </div>
+        <!-- End Section introduction and search bar  -->
+    </div>
+    <!-- End Hero bar -->
 
 
-        <!--jQuery and Bootstrap Bundle -->
-        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    <!--jQuery and Bootstrap Bundle -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 </body>
 
 </html>
