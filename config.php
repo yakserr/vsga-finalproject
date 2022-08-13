217d78f73bc70dc636ac30bdeed9144a8d8e6b4e
Yassir Ah <yassirnugroho48@gmail.com>
2022-08-13 21:47:44 +0700

    add connection to database in config

--- config.php
+++ config.php
@@ -1,4 +1,17 @@
 <?php
 
-// Base path for the website
-define('BASE', __DIR__);
+// App name
+define('APP_NAME', 'VSGA Final Project!');
+
+// base folder project
+define('BASE', 'http://localhost/phpproject/vsga-finalproject');
+
+
+// Connection to databse mysql
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
+$db = 'vsga_finalproject';
+
+
+$conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n" . $conn->error);
